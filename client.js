78ef6578af9e656927333b4f0f508f559c23458d
Halima Omar <omarhalima100@gmail.com>
2023-04-05 20:59:48 -0500

    completed question 5

--- client.js
+++ client.js
@@ -6,7 +6,10 @@
  * 3. After the loop, log out the total number of
  *    hobbies
  */
-
+ let hobbies = ['video games', 'cooking', 'exercising', 'restaurants'];
+ for (let hobby of hobbies){
+    console.log(hobby)
+ }
 
 // Example output
 // 1. swimming
@@ -24,8 +27,12 @@
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
-
-
+let colors = [ 'blue', 'green' , 'teal']
+console.log(colors);
+let tealCount=[2];
+for (let tealCount of colors){
+    console.log(tealCount);
+}
 // Example output
 // green, red, teal, orange, teal
 // Teal was found 2 times
@@ -40,8 +47,15 @@
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
+let Numbers = ['1','2','3','4','5'];
+console.log(Numbers);
+let oddNumbers =[];
+let evenNumbers = [];
+for (let i = 0; i < Numbers.length; i++) {
+    console.log(number);
+}
 
-
+    //console.log;g
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
 // Odd 3, 7, 11
@@ -57,7 +71,11 @@
  * 4. Output both arrays
  */
 
-
+let boolean =['true', 'false' ,'false','true'];
+console.log(boolean);
+let emptyArray = [];
+console.log(emptyArray);
+for(let false of boolean)
 // Example output
 // true, false, true, true
 // Toggled false, true, false, false
@@ -72,6 +90,14 @@
  *    NOTE: You should not need a second array here.
  * 3. Output the array
  */
+let Sequence = ['3','0','2','8','0','0','0'];
+console.log(Sequence);
+for(let i = 0; i < Sequence.length; i++);{
+    Sequence.pop();
+}
+console.log(Sequence);
+
+
 
 
 // Example output
